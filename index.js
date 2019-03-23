f065bd148f99c19455348d7383dea29c11ef54b5
xtazxz <xtazxz@qq.com>
2019-03-24 00:14:58 +0800

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "defaultSeverity": "error",
+    "extends": [
+        "tslint:recommended"
+    ],
+    "jsRules": {},
+    "rules": {
+        "max-line-length": {
+            "options": [
+                1000
+            ]
+        },
+        "eofline": false,
+        "no-console": false,
+        "ordered-imports": false,
+        "variable-name": false,
+        "trailing-comma": false
+    },
+    "rulesDirectory": []
+}
